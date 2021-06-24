4837c8cfe6cba4322d32103df70a8de9039d6e4e
TakeshiOgimi <viridia.lost.yukitsubaki@gmail.com>
2021-06-24 19:02:20 +0900

    室温と湿度をグラフ表示するようにしました

--- components/organisms/Roomstate.tsx
+++ components/organisms/Roomstate.tsx
@@ -1,4 +1,12 @@
 import {FC, useEffect, useState} from 'react'
+/**
+ * 下記コメントアウトのインポートを使うと window is not defined で怒られるので
+ * 下記URLに記載がある dynamic Import で解決
+ * https://zenn.dev/ninomium/articles/5fed8af69640a6
+ */
+// import Chart from 'react-apexcharts'
+import dynamic from 'next/dynamic';
+const ReactApexChart = dynamic(() => import('react-apexcharts'), {ssr: false})
 
 const Roomstate:FC = () => {
   // 最新室温情報を取得
@@ -25,24 +33,79 @@ const Roomstate:FC = () => {
   },[])
 
   // グラフ用の情報を更新(最新30件分のデータを保持するように調整する処理をする)
-  const [statusList, setStateList] = useState([])
+  const [statusList, setStatusList] = useState([])
+  const [chartOption, setChartOption] = useState({option: {}, series: []});
   useEffect(() => {
     // APIの情報を整形
     // TODO: newest_eventsは後で定義する
     const newestEvents = status[0]?.newest_events || {}
+    const datetime = Date.now()
     const humidity = newestEvents?.hu?.val || 0
     const illumination = newestEvents?.il?.val || 0
     const movement = newestEvents?.mo?.val || 0
     const temperature = newestEvents?.te?.val || 0
-    const newStatus = {humidity, illumination, movement, temperature}
+    const newStatus = {datetime, humidity, illumination, movement, temperature}
 
     // 最新情報を30件分の情報のみ登録する
     const newStatusList = statusList
     newStatusList.push(newStatus)
-    if(newStatusList.length > 30) newStatusList.pop()
-    setStateList(newStatusList)
-  }, [status])
+    if(newStatusList.length > 30) newStatusList.shift()
+    setStatusList(newStatusList)
 
+    // 以下の処理は分けるべき？
+    // 実行時間をリストを生成
+    const datetimeList = newStatusList.map(v => v?.datetime || Date.now())
+    // 室温のリストを生成
+    const temperatureList = newStatusList.map(v => v?.temperature || 0)
+    // 湿度
+    const humidityList = newStatusList.map(v => v?.humidity || 0)
+    const newOption = {
+      options: {
+        chart: {
+          height: '320',
+          width: '500',
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: datetimeList
+        },
+        yaxis:[
+          {
+            title: {
+              text: '温度',
+            },
+          },
+          {
+            opposite: true,
+            title: {
+              text: '湿度',
+            },
+          },
+        ],
+        tooltip: {
+          x: {
+            format: 'dd/MM/yy HH:mm'
+          },
+        },
+      },
+      series:[
+        {
+          name: 'Temperature',
+          type: 'area',
+          data: temperatureList
+        },
+        {
+          name: 'humidity',
+          type: 'line',
+          data: humidityList
+        }
+      ]
+    }
+    setChartOption(newOption)
+  }, [status])
   
   const [count, setCount] = useState(0)
   useEffect(() => {
@@ -57,12 +120,13 @@ const Roomstate:FC = () => {
     }
   }, [])
 
-  // console.log('state:', state)
   return (
     <div>
       <p>{count} sec</p>
-      <p>{statusList.length}</p>
       <p>{JSON.stringify(statusList)}</p>
+      <div>
+        <ReactApexChart options={chartOption?.options || {}} series={chartOption?.series || []}/>
+      </div>
     </div>
   )
 }
